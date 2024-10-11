da0f6e8fbbdd25e767000940564c6175402ad186
AlejandraAcuña <173119479+AlejandraAcu@users.noreply.github.com>
2024-10-11 14:19:26 -0500

    user-verifed

--- src/middlewares/emailCode.middlewares.js
+++ src/middlewares/emailCode.middlewares.js
@@ -1,31 +1,29 @@
 const EmailCode = require('../models/EmailCode')
-const sendEmail = require('../utils/sendEmail')
+const { sendEmail } = require('../utils/sendEmail')
 
 async function emailCode(req, res, next) {
 
     const code = require('crypto').randomBytes(64).toString('hex')
 
-    const {id, email, firstName} = req.result
-    
+    const { id, email, firstName } = req.result
+
     const userId = id
 
-    const body = { code, userId}
+    const body = { code, userId }
 
     const userCode = await EmailCode.create(body)
-    if(!userCode) return res.sendStatus(400)
-
+    if (!userCode) return res.sendStatus(400)
+        
     sendEmail({
-        to: email, 
-        subject:'test',
-        html:`
+        to: email,
+        subject: 'test',
+        html: `
             <div>
                 <h1>Hola ${firstName}</h1>
                 <p> y tu codigo para verificar la cuenta es ${code}
             </div>
         `
     })
-
     return res.status(201).json('User Created')
 }
-
 module.exports = emailCode
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,31 +1,25 @@
-const nodemailer = require("nodemailer");
-
-const sendEmail = (options) => new Promise((resolve, reject) => {
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        },
-				tls:{
-						rejectUnauthorized:false
-        },
-				secure:false
-    })
-    const mailOptions = {
-        from: process.env.EMAIL,
-        ...options
+const nodemailer = require('nodemailer');
+const sendEmail = (options) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
+    },
+    tls: {
+      rejectUnauthorized: false
+    },
+  });
+  const mailOptions = {
+    from: process.env.EMAIL,
+    ...options
+  }
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
     }
-    transporter.sendMail(mailOptions, (error, info) => {
-        console.log(error, info)
-        if (error) {
-            //console.log(error);
-            console.log('me ejecute soy el error')
-            return reject({ message: "An error has occured" })
-        }
-        return resolve({ message: "Email sent successfully" })
-    })
-})
-
-module.exports = sendEmail;
+  });
+}
+module.exports = { sendEmail }
